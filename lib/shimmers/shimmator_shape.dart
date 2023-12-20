9110fcd0cf7de38d66c7f9e9caf80b93fbdae432
SivaGandrikala <siva.gandrikala@fleetcor.com>
2023-12-20 19:16:55 +0530

    Add shimmer to common resources package

---
+++ lib/shimmers/shimmator_shape.dart
@@ -0,0 +1,53 @@
+part of shimmer_module;
+
+class ShimmatorShape extends StatelessWidget {
+  final double width;
+  final double height;
+  final ShapeBorder shapeBorder;
+  final bool isPurple;
+
+  const ShimmatorShape.rectangular({
+    required this.height,
+    this.width = double.infinity,
+    this.isPurple = false,
+  }) : shapeBorder = const RoundedRectangleBorder();
+
+  const ShimmatorShape.roundedRectangular({
+    required this.height,
+    this.width = double.infinity,
+    this.isPurple = false,
+  }) : shapeBorder = const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(16)));
+
+  const ShimmatorShape.circular({
+    required this.height,
+    this.width = double.infinity,
+    this.isPurple = false,
+    this.shapeBorder = const CircleBorder(),
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    Color baseColor = const Color(0xFFF0F0F0);
+    Color highlightColor = Colors.grey[300]!;
+    Color shapeColor = Colors.grey[100]!;
+
+    if (isPurple) {
+      baseColor = Colors.purple[100]!;
+      highlightColor = DrivenColors.brandPurple;
+      shapeColor = Colors.purple[100]!;
+    }
+    return Shimmer.fromColors(
+      baseColor: baseColor,
+      highlightColor: highlightColor,
+      child: Container(
+        width: width,
+        height: height,
+        decoration: ShapeDecoration(
+          color: shapeColor,
+          shape: shapeBorder,
+        ),
+      ),
+    );
+  }
+}
