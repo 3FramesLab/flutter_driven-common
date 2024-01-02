295ff0d19f93429b5207d88e5d6c54ec714187c2
Smeet Bhat <Smeet.Bhat@fleetcor.com>
2024-01-02 12:45:59 +0530

    Add shared preferences instance in Globals.dart

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,6 +1,7 @@
 import 'package:driven_common/analytics/aep_core.dart';
 import 'package:driven_common/analytics/driven_analytics.dart';
 import 'package:driven_common/dynatrace/driven_dynatrace.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Globals {
   late DrivenDynatrace dynatrace;
@@ -10,12 +11,14 @@ class Globals {
   late bool isComdata;
   late String packageId;
   late String androidCertSignature;
+  late SharedPreferences sharedPreferences;
 
-  void init({required String flavor, bool isCardHolderLogin = false}) {
+  void init({required String flavor, bool isCardHolderLogin = false}) async {
     appFlavor = flavor;
     isCardHolderLogin = isCardHolderLogin;
     initializeAnalytics();
     initializeDynatrace();
+    await initializeSharedPreferences();
   }
 
   void initializeAnalytics() {
@@ -24,5 +27,9 @@ class Globals {
 
   void initializeDynatrace() {
     dynatrace = DrivenDynatrace.init();
+  }
+
+  Future<void> initializeSharedPreferences() async {
+    sharedPreferences = await SharedPreferences.getInstance();
   }
 }
