9c0f5e0798802ca5b53ce64e026b76d7251b8060
Smeet Bhat <Smeet.Bhat@fleetcor.com>
2024-01-16 13:19:02 +0530

    Made DrivenCommon singleton and pass baseUrl

--- lib/driven_common.dart
+++ lib/driven_common.dart
@@ -1,16 +1,25 @@
+import 'package:driven_common/data/data_sources/remote/api_constants.dart';
 import 'package:driven_common/globals.dart';
 import 'package:flutter/foundation.dart';
 
 import 'driven_common_platform_interface.dart';
 
 class DrivenCommon {
+  DrivenCommon._internal();
+  static final DrivenCommon _instance = DrivenCommon._internal();
+  static DrivenCommon get instance => _instance;
+
   Future<String?> getPlatformVersion() {
     return DrivenCommonPlatform.instance.getPlatformVersion();
   }
 
-  Future<void> init({required String appFlavor}) async {
+  Future<void> init({
+    required String appFlavor,
+    required String baseUrl,
+  }) async {
     try {
       debugPrint('driven common package: init() called');
+      ApiConstants.instance.baseUrl = baseUrl;
       Globals().init(flavor: appFlavor);
     } on Exception catch (_) {
       debugPrint('Error while initializing common resources packages');
