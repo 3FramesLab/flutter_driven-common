c3a8fddca31355f1d648e99c9de28abd0b56d13c
Hitesh.Danidhariya <hitesh.danidhariya@fleetcor.com>
2024-03-11 17:51:55 +0530

    UCX-25733 10 Digits allow for prop card

--- lib/driven_components/form_widgets/validators/card_number_characters_validator.dart
+++ lib/driven_components/form_widgets/validators/card_number_characters_validator.dart
@@ -2,14 +2,18 @@ part of driven_components_module;
 
 class CardNumberCharactersValidator extends Validator {
   final int n;
+  final int propCardLength;
 
-  const CardNumberCharactersValidator(this.n) : super();
+  const CardNumberCharactersValidator(this.n, this.propCardLength) : super();
 
   @override
   bool isValid(String? value) {
-    return value != null && value.length >= n;
+    if (value != null) {
+      return value.length == propCardLength || value.length >= n;
+    }
+    return false;
   }
 
   @override
-  String get failMessage => Validation.atLeastNCharacters(16);
+  String get failMessage => Validation.notValidCardNumber;
 }
