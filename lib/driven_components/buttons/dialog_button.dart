6250a9d0b76f80d5ab1ef3c7da7230c818d2b925
Logesh.Sharma <Logesh.Sharma@fleetcor.com>
2024-04-04 20:38:09 +0530

    update background color in DialogButton

--- lib/driven_components/buttons/dialog_button.dart
+++ lib/driven_components/buttons/dialog_button.dart
@@ -5,13 +5,14 @@ class DialogButton extends RoundedButton {
     required VoidCallback? onPressed,
     required String text,
     double? height,
+    Color? backgroundColor,
     super.key,
   }) : super(
           onPressed: onPressed,
           text: text,
           height: height ?? 42,
           buttonTextStyle: f16SemiBoldWhite,
-          backgroundColor: DrivenColors.primaryButtonColor,
+          backgroundColor: backgroundColor ?? DrivenColors.primaryButtonColor,
           foregroundColor: DrivenColors.primaryButtonTextColor,
         );
 }
