94047e045466c1000d0ed68a563347c1aa352dad
Shailendra Madda <Shailendra.Madda@fleetcor.com>
2024-08-21 21:10:35 +0530

    UCX-30684 update components for Amazon

--- lib/driven_components/form_widgets/validators/has_n_digits_validator.dart
+++ lib/driven_components/form_widgets/validators/has_n_digits_validator.dart
@@ -2,14 +2,20 @@ part of driven_components_module;
 
 class HasNDigitsValidator extends Validator {
   final int n;
+  final String customErrorMessage;
 
-  const HasNDigitsValidator(this.n) : super();
+  const HasNDigitsValidator(this.n, {this.customErrorMessage = ''}) : super();
 
   @override
   bool isValid(String? value) {
-    return value != null && value.length == n;
+    return value != null && value.removeAllWhitespace.length == n;
   }
 
   @override
-  String get failMessage => Validation.mustBeNDigits(n);
+  String get failMessage {
+    if (customErrorMessage != '') {
+      return Validation.customError(customErrorMessage);
+    }
+    return Validation.mustBeNDigits(n);
+  }
 }
