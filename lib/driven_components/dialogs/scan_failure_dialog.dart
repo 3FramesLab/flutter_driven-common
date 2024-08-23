45e969c2c7559f5b6c1cfcb6e86eeeb592e17802
SmeetBhatt <smeet.bhatt@3frameslab.com>
2024-08-23 14:19:37 +0530

    Add ScanFailureDialog

---
+++ lib/driven_components/dialogs/scan_failure_dialog.dart
@@ -0,0 +1,59 @@
+part of driven_components_module;
+
+class ScanFailureDialog extends StatelessWidget {
+  final VoidCallback? onPrimaryButtonTap;
+  final VoidCallback? onSecondaryButtonTap;
+  final String primaryButtonText;
+  final String secondaryButtonText;
+  final String title;
+
+  const ScanFailureDialog({
+    required this.title,
+    required this.primaryButtonText,
+    required this.secondaryButtonText,
+    this.onPrimaryButtonTap,
+    this.onSecondaryButtonTap,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return PopScope(
+      onPopInvoked: (_) => Future.value(false),
+      child: Dialog(
+        insetPadding: const EdgeInsets.all(8),
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+          constraints: const BoxConstraints(minHeight: 248),
+          child: DrivenColumn(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Flexible(child: _titleText),
+              _primaryButton,
+              _secondaryButton,
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget get _titleText => Text(
+        title,
+        textAlign: TextAlign.center,
+        style: f14RegularBlack,
+      );
+
+  Widget get _primaryButton => PrimaryButton(
+        onPressed: onPrimaryButtonTap,
+        text: 'Scan Again',
+        backgroundColor: DrivenColors.primaryAmazon,
+      );
+
+  Widget get _secondaryButton => ClickableText(
+        title: 'Enter Card Number Manually',
+        onTap: onSecondaryButtonTap,
+        textColor: DrivenColors.primaryAmazon,
+      );
+}
