2b546e5eb2eb132b669fe71eaa77640d0f55cadd
SmeetBhatt <smeet.bhatt@3frameslab.com>
2024-09-10 12:03:23 +0530

    Update space_after_every_four_input_formatter.dart

--- lib/driven_components/form_widgets/text_formatters/space_after_every_four_input_formatter.dart
+++ lib/driven_components/form_widgets/text_formatters/space_after_every_four_input_formatter.dart
@@ -4,14 +4,26 @@ class SpaceAfterEveryFourInputFormatter extends TextInputFormatter {
   @override
   TextEditingValue formatEditUpdate(
       TextEditingValue oldValue, TextEditingValue newValue) {
-    final String newText = newValue.text
-        .replaceAll(' ', '')
-        .replaceAllMapped(RegExp('.{1,4}'), (match) => '${match.group(0)} ')
-        .trim();
+    String newText = newValue.text.replaceAll(' ', '');
+    int selectionIndex = newValue.selection.end;
+    String formattedText = '';
+    int nonSpaceCount = 0;
+
+    for (int i = 0; i < newText.length; i++) {
+      if (nonSpaceCount > 0 && nonSpaceCount % 4 == 0) {
+        formattedText += ' ';
+        if (i < selectionIndex) {
+          selectionIndex++;
+        }
+      }
+
+      formattedText += newText[i];
+      nonSpaceCount++;
+    }
 
     return TextEditingValue(
-      text: newText,
-      selection: TextSelection.collapsed(offset: newText.length),
+      text: formattedText,
+      selection: TextSelection.collapsed(offset: selectionIndex),
     );
   }
 }
