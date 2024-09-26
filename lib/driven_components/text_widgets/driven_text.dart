ad13ee28c8ea2ca8c281c6c28bd515c8a8174188
SmeetBhatt <smeet.bhatt@3frameslab.com>
2024-09-26 20:10:09 +0530

    Remove camera and google ml text recognitions

--- lib/driven_components/text_widgets/driven_text.dart
+++ lib/driven_components/text_widgets/driven_text.dart
@@ -3,10 +3,13 @@ part of driven_components_module;
 class DrivenText extends StatelessWidget {
   final TextStyle? style;
   final String text;
+  final TextAlign? textAlign;
+
   const DrivenText({
     super.key,
     required this.text,
     this.style = f16RegularBlack,
+    this.textAlign,
   });
 
   @override
@@ -14,6 +17,7 @@ class DrivenText extends StatelessWidget {
     return Text(
       text,
       style: style,
+      textAlign: textAlign,
     );
   }
 }
