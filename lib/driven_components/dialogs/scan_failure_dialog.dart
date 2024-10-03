2e64f5b070721b6f0b031b96ba97a46aed9909af
SmeetBhatt <smeet.bhatt@3frameslab.com>
2024-10-03 19:18:51 +0530

    Update scan_failure_dialog.dart

--- lib/driven_components/dialogs/scan_failure_dialog.dart
+++ lib/driven_components/dialogs/scan_failure_dialog.dart
@@ -3,18 +3,26 @@ part of driven_components_module;
 class ScanFailureDialog extends StatelessWidget {
   final VoidCallback? onPrimaryButtonTap;
   final VoidCallback? onSecondaryButtonTap;
+  final Widget? icon;
   final String primaryButtonText;
   final String secondaryButtonText;
-  final List<TextSpan> text;
+  final String titleText;
+  final TextStyle titleTextStyle;
+  final String? subTitleText;
+  final TextStyle? subTitleTextStyle;
   final VoidCallback? onPopInvoked;
 
   const ScanFailureDialog({
-    required this.text,
+    required this.titleText,
+    required this.titleTextStyle,
     required this.primaryButtonText,
     required this.secondaryButtonText,
+    this.subTitleText,
+    this.subTitleTextStyle,
     this.onPrimaryButtonTap,
     this.onSecondaryButtonTap,
     this.onPopInvoked,
+    this.icon,
     super.key,
   });
 
@@ -33,13 +41,15 @@ class ScanFailureDialog extends StatelessWidget {
           insetPadding: const EdgeInsets.all(8),
           child: Container(
             padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
-            constraints: const BoxConstraints(minHeight: 208),
+            constraints: BoxConstraints(minHeight: icon == null ? 208 : 248),
             child: DrivenColumn(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               mainAxisSize: MainAxisSize.min,
               children: [
-                Flexible(child: _titleText),
+                _label,
+                const SizedBox(height: 8),
                 _primaryButton,
+                const SizedBox(height: 8),
                 _secondaryButton,
               ],
             ),
@@ -49,18 +59,33 @@ class ScanFailureDialog extends StatelessWidget {
     );
   }
 
-  Widget get _titleText => Text.rich(
-        TextSpan(
-          style: const TextStyle(
-            color: DrivenColors.black90,
-            fontSize: 15,
-            fontWeight: DrivenFonts.fontWeightRegular,
-            height: 1.4,
-          ),
-          children: text,
-        ),
+  Widget get _label => Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          if (icon != null) icon!,
+          Flexible(child: _titleText),
+          _subTitleText(),
+        ],
+      );
+
+  Widget get _titleText => Text(
+        titleText,
+        style: titleTextStyle,
         textAlign: TextAlign.center,
       );
+
+  Widget _subTitleText() {
+    if (subTitleText == null) {
+      return const SizedBox.shrink();
+    } else {
+      return Flexible(
+          child: Text(
+        subTitleText!,
+        style: subTitleTextStyle ?? f14RegularBlack,
+        textAlign: TextAlign.center,
+      ));
+    }
+  }
 
   Widget get _primaryButton => PrimaryButton(
         onPressed: onPrimaryButtonTap?.call,
