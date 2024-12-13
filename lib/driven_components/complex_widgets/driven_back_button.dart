0644d3b9ae3200623259b27de27b228281a26b4b
Shailendra Madda <Shailendra.Madda@fleetcor.com>
2024-12-13 18:16:45 +0530

    UCX-33819 Update driven back button

--- lib/driven_components/complex_widgets/driven_back_button.dart
+++ lib/driven_components/complex_widgets/driven_back_button.dart
@@ -4,17 +4,19 @@ class DrivenBackButton extends StatelessWidget {
   final void Function()? onPressed;
   final Color color;
   final MainAxisSize mainAxisSize;
-  final double width;
-  final String? backButtonText;
+  final double iconWidth;
+  final String? buttonText;
+  final double buttonTextWidth;
 
   const DrivenBackButton({
     super.key,
     this.onPressed,
-    this.backButtonText = DrivenConstants.back,
+    this.buttonText = DrivenConstants.back,
     this.color = DrivenColors.primary,
     this.mainAxisSize = MainAxisSize.max,
     // TO-DO (Shailendra): Make it 24 as default once we remove admin_template in super-app
-    this.width = 17,
+    this.iconWidth = 17,
+    this.buttonTextWidth = 42,
   });
 
   @override
@@ -28,16 +30,16 @@ class DrivenBackButton extends StatelessWidget {
         mainAxisSize: mainAxisSize,
         children: [
           SizedBox(
-            width: width,
+            width: iconWidth,
             child: Icon(
               Icons.navigate_before,
               color: color,
             ),
           ),
           SizedBox(
-            width: 42,
+            width: buttonTextWidth,
             child: Text(
-              backButtonText.toString(),
+              buttonText.toString(),
               style: f16SemiboldPrimary.copyWith(color: color),
               textScaler: const TextScaler.linear(1),
             ),
